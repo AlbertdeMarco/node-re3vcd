3f786164fcf4f8ecf252338b52141460f0cbbc96
AlbertdeMarco <albertdemarco10@gmail.com>
2021-11-06 20:20:53 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# node-re3vcd
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/node-re3vcd)
---
+++ index.js
@@ -0,0 +1,23 @@
+# Actividad Guiada 03
+# Albert de Marco
+
+<head>
+
+<script src="code.jquery.com/jquery-3.3.1.min.js"></script>
+
+<script src="cdnjs.cloudflare.com/ajax/libs/d3/3.5.16/d3.min.js"></script>
+
+<script src="cdnjs.cloudflare.com/ajax/libs/d3plus/1.9.2/d3plus.full.min.js"></script>
+
+</head>
+
+d3.csv("https://raw.githubusercontent.com/mwaskom/seaborn-data/master/titanic.csv", function(error, data) {
+if(error){console.log("Error al leer el fichero titanic.csv");
+} else {
+//Incorporación de gráficos
+// .....
+}
+});
+
+<h2> Gráfico de Columnas con D3Plus</h2>
+<div id="viz1"></div>
